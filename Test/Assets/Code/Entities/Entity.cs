6442c1e01d8f6608464818d3cefc93588b0d78fa
schaffer2013 <schaffer2013@gmail.com>
2016-03-06 10:12:51 -0500

    Initial Commit

---
+++ Test/Assets/Code/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Entity : MonoBehaviour {
+
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
---
+++ Test/Assets/Code/Entities/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Entity {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			rigidbody2D.transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			rigidbody2D.transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			rigidbody2D.transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			rigidbody2D.transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+	}
+}
